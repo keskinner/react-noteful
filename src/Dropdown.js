85738ddc51db80f703f59699dd48bdd3f683401d
Kevin Skinner <nivek.skinner@gmail.com>
2020-01-27 15:10:35 -0800

    implementing add folder, add note, and delete note feature

---
+++ src/Dropdown.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ApiContext from './ApiContext';
+import './Dropdown.css';
+
+export default class Dropdown extends React.Component {
+    static contextType = ApiContext;
+    
+    render() {
+        const {folders = []} = this.context;
+        return (
+            <select 
+                className = "dropdown"
+                onChange = {e => this.props.updateFolderId(e.target.value)}>
+                {folders.map(folder => 
+                    <option 
+                        key = {folder.name}
+                        value = {folder.id}
+                        className = "folderOption">
+                        {folder.name}
+                    </option>
+                    )}
+            </select> 
+        )
+    }
+}
---
+++ src/NotefulForm/NotefulForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NotefulForm.css'
+
+export default function NotefulForm(props) {
+  const { className, ...otherProps } = props
+  return (
+    <form
+      className={['Noteful-form', className].join(' ')}
+      action='#'
+      {...otherProps}
+    />
+  )
+}
+
+NotefulForm.propTypes = {
+  className: PropTypes.string.isRequired
+};
